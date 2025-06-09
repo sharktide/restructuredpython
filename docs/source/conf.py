5d5d23dcd7020fbed8351c0b4b41247e488af4d9
Rihaan Meher <meherrihaan@gmail.com>
2025-06-09 19:17:35 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,8 +51,9 @@ html_theme_options = {
     "announcement": '''
     
     <nav class="navbar">
-    <div class="logo">reStructuredPython</div>
+    <a href="/"><div class="logo">reStructuredPython</div></a>
     <ul class="nav-links">
+      <li><a href="https://restructuredpython.rf.gd/playground">Playground</a></li>
       <li><a href="https://restructuredpython.rf.gd#github">Open Source</a></li>
       <li><a href="https://restructuredpython.rf.gd#features">Features</a></li>
       <li><a href="/">Docs</a></li>
